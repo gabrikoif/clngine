3529b956dec817b4331ddab2b2ca86cc0051ef10
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-02 18:14:02 +0300

    feat: 3d fully implemented

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "shader.h"
 #include "meshutils.h"
 #include "meshgeometry.h"
+#include "camera.h"
 
 int main()
 {
@@ -46,13 +47,15 @@ int main()
         return -1;
     }
     glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
 
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
+
     // glViewport(width, height);
 
     // vertices
-    Mesh sphere = MeshGen::CreateSphere(0.5f);
+    Mesh sphere = MeshGen::CreateSphere(2.0f);
 
     // clang-format off
     // clang-format on
@@ -62,21 +65,42 @@ int main()
 
     Shader shader("./shader/vertex.glsl", "./shader/fragment.glsl");
     GLint colorLoc = shader.getLocation("uColor");
+    GLint modelLoc = shader.getLocation("model");
+    GLint viewLoc = shader.getLocation("view");
+    GLint projectionLoc = shader.getLocation("projection");
     shader.use();
+
+    Camera camera;
+    camera.fov = 45.0f;
+    camera.farPlane = 1000.0f;
+    camera.nearPlane = 1.0f;
+    globalCamPointer = &camera;
 
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         {
             break;
         }
 
+        handle_input(window);
+
+        float aspectRatio = (float)width / (float)height;
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(10, 5, 0));
+        glm::mat4 view = camera.genView();
+        glm::mat4 projection = camera.genProj(aspectRatio);
+
         shader.use();
-        shader.setVec3("uColor", glm::vec3(1.0f, 0.0f, 0.0f), colorLoc);
+        shader.setMat4("model", model, modelLoc);
+        shader.setMat4("view", view, viewLoc);
+        shader.setMat4("projection", projection, projectionLoc);
+        shader.setVec3("uColor", glm::vec3(1.0f, 1.0f, 1.0f), colorLoc);
         glBindVertexArray(readymesh.VAO);
-        glDrawElements(GL_TRIANGLE_FAN, readymesh.indexCount, GL_UNSIGNED_INT, (void*)0);
+        glDrawElements(GL_TRIANGLES, readymesh.indexCount, GL_UNSIGNED_INT, (void*)0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
