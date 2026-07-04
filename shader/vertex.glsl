66a67e88d6ff01894665c50ffaadffd323694225
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-04 17:11:26 +0300

    feat: vert shader implement VBO model matrix data and frag shader gets colors from VBO colors data

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,12 +1,21 @@
 #version 330 core
 
+// Standard attributes from your mesh
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoords; 
+layout (location = 2) in vec3 aNormal;
 
-uniform mat4 model;
+// The batched instanced matrix (takes up 4 slots)
+layout (location = 3) in mat4 aInstanceMatrix; 
+layout (location = 7) in vec3 aInstanceColor;
+
+out vec3 uColor;
+
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
-    gl_Position = projection * view * model * vec4(aPos, 1.0); 
+void main() {
+    // aInstanceMatrix is the model matrix.
+    gl_Position = projection * view * aInstanceMatrix * vec4(aPos, 1.0);
+    uColor = aInstanceColor;
 }
