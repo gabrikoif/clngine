461415b0d8d07c5331ab4657c749e82c49adb186
gabriarch <koifman.gabriel@gmail.com>
2026-07-05 22:39:18 +0300

    chore: rename .h includes to .hpp if it includes classes and fix inside dependent includes too

--- include/input.h
+++ include/input.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
-#include "camera.h"
+#include "camera.hpp"
 
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
