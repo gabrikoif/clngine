49fc7e4676029ecba3e5644fe938ac0d06bbdefa
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-11 16:37:22 +0300

    optimization: lower default rings count for the sphere MeshGen
    Too many redundant vertices for a non noticable difference.

--- include/meshgeometry.hpp
+++ include/meshgeometry.hpp
@@ -8,6 +8,6 @@ namespace MeshGen {
     // Function declarations
     Mesh CreateQuad(float width = 1.0f, float height = 1.0f);
     Mesh CreateCube(float size = 1.0f); 
-    Mesh CreateSphere(float radius = 1.0f, unsigned int segments = 24, unsigned int rings = 24);
+    Mesh CreateSphere(float radius = 1.0f, unsigned int segments = 24, unsigned int rings = 16);
 
 };
