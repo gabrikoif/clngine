49d040ea4f7edc5382dbc9d858ceded8deb7ac7b
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-13 22:12:34 +0300

    refactor: handleMouseMovement and handleKeyboard
    Now, there are two methods to very easily handle input meant for the camera.

--- include/camera.hpp
+++ include/camera.hpp
@@ -7,22 +7,37 @@ class Camera
 {
 public:
     // Constructor with default argument
-    Camera(glm::vec3 startPos = glm::vec3(0.0f, 0.0f, 0.0f));
+    Camera(
+        glm::vec3 p_pos = glm::vec3(0.0f),
+        glm::vec3 p_dir = glm::vec3(0.0f, 0.0f, -1.0f),
+        float p_fov = 100.0f,
+        float p_nearPlane = 0.1f,
+        float p_farPlane = 1000.0f,
+        float p_speed = 4.0f,
+        bool p_fly = true);
 
-    glm::vec3 cameraPos;
-    glm::vec3 viewDir;
-
-    float fov;
-    float nearPlane;
-    float farPlane;
+    glm::vec3 m_pos;
+    bool m_fly;
 
     // const means these functions promise not to alter cameraPos, yaw, pitch, etc.
     glm::mat4 genView() const;
-    glm::mat4 genProj(float aspectRatio) const; 
+    glm::mat4 genProj(float aspectRatio) const;
 
     void handleMouseMovement(float xOffset, float yOffset);
+    void handleKeyboard(bool w, bool a, bool s, bool d, float DeltaTime = 0.016f);
 
 private:
     float yaw;
     float pitch;
+    float m_fov;
+    float m_nearPlane;
+    float m_farPlane;
+    float m_speed;
+
+    glm::vec3 m_forward;
+    glm::vec3 m_right;
+
+    glm::vec3 m_move_forward;
 };
+
+inline constexpr glm::vec3 glUp = glm::vec3(0.0f, 1.0f, 0.0f);
