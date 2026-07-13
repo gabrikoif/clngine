eb6eaa0ea16f945221ecefaf20735e7a1c98ac3a
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-13 22:15:17 +0300

    refactor and implement: all smaller refactors included.
    Using previous commits, engine code is now shorter and cleaner(by 50 lines)

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,17 +3,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <vector>
-#include <iostream>
-#include <random>
-#include <memory>
-
 #include "input.h"
 #include "camera.hpp"
 #include "shader.hpp"
+
 #include "meshgeometry.hpp"
-#include "meshutils.hpp"
-#include "data.hpp"
+#include "instanced_mesh_group.hpp"
 
 class Engine
 {
@@ -31,25 +26,14 @@ private:
     void update(float deltaTime = 0.016f);
     void render();
 
+    bool isPressed(int key);
+
+    GLFWwindow* m_window = nullptr;
     bool m_isRunning;
-    GLFWwindow* m_window;
-    int m_width;
-    int m_height;
+    int m_width, m_height;
 
-    MeshUtils::GLMesh m_mesh;
-    int m_meshCount;
-    float m_bounds;
-
-    GLuint m_vao;
-    GLuint m_vbo;
-    bool m_needsGPUUpdate = true;
-
-    Shader m_shader;
     Camera m_camera;
+    Shader m_shader;
 
-    GLBuffer<glm::mat4> m_modelbuffer;
-    GLBuffer<glm::vec3> m_colorbuffer;
-
-    std::vector<glm::vec3> m_colors;
-    std::vector<glm::mat4> m_models;
+    InstancedMeshGroup m_group;
 };
