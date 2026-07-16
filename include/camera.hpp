992658a8a9bd08e01e29d81c15221a8aff546ca7
gabrielkoifman <koifman.gabriel@gmail.com>
2026-07-16 23:02:19 +0300

    refactor(camera): change default values in the constructor + format doc

--- include/camera.hpp
+++ include/camera.hpp
@@ -6,38 +6,35 @@
 class Camera
 {
 public:
-    // Constructor with default argument
-    Camera(
-        float p_speed = 4.0f,
-        glm::vec3 p_pos = glm::vec3(0.0f),
-        glm::vec3 p_dir = glm::vec3(0.0f, 0.0f, -1.0f),
-        float p_fov = 100.0f,
-        float p_nearPlane = 0.1f,
-        float p_farPlane = 1000.0f,
-        bool p_fly = true);
+  // Constructor with default argument
+  Camera(float p_speed = 4.0f, glm::vec3 p_pos = glm::vec3(0.0f, 2.0f, 0.0f),
+         glm::vec3 p_dir = glm::vec3(0.0f, 0.0f, -1.0f), float p_fov = 100.0f,
+         float p_nearPlane = 0.1f, float p_farPlane = 1000.0f,
+         bool p_fly = false);
 
-    glm::vec3 m_pos;
-    bool m_fly;
+  glm::vec3 m_pos;
+  bool m_fly;
 
-    // const means these functions promise not to alter cameraPos, yaw, pitch, etc.
-    glm::mat4 genView() const;
-    glm::mat4 genProj(float aspectRatio) const;
+  // const means these functions promise not to alter cameraPos, yaw, pitch,
+  // etc.
+  glm::mat4 genView() const;
+  glm::mat4 genProj(float aspectRatio) const;
 
-    void handleMouseMovement(float xOffset, float yOffset);
-    void handleKeyboard(bool w, bool a, bool s, bool d, float DeltaTime = 0.016f);
+  void handleMouseMovement(float xOffset, float yOffset);
+  void handleKeyboard(bool w, bool a, bool s, bool d, float DeltaTime = 0.016f);
 
 private:
-    float yaw = -90.0f;
-    float pitch = 0.0f;
-    float m_fov;
-    float m_nearPlane;
-    float m_farPlane;
-    float m_speed;
+  float yaw = -90.0f;
+  float pitch = 0.0f;
+  float m_fov;
+  float m_nearPlane;
+  float m_farPlane;
+  float m_speed;
 
-    glm::vec3 m_forward;
-    glm::vec3 m_right;
+  glm::vec3 m_forward;
+  glm::vec3 m_right;
 
-    glm::vec3 m_move_forward;
+  glm::vec3 m_move_forward;
 };
 
 inline constexpr glm::vec3 glUp = glm::vec3(0.0f, 1.0f, 0.0f);
